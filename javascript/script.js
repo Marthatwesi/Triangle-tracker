e0e6b4234654f25bc272d1aaa0729369b2f20b39
twesigyemartha <twesimartha@gmail.com>
2019-03-23 17:33:21 +0300

    added variable B

--- javascript/script.js
+++ javascript/script.js
@@ -2,4 +2,6 @@ function triangle() {
   var sideA = parseInt(document.getElementById('a').value);
   console.log(sideA);
   // console.log(sideA);
-  
+    var sideB = parseInt(document.getElementById('b').value);
+    console.log(sideB);
+    // console.log(sideB);
